d63231bfe226d25999ea2f1f7066ae1da7963cfa
vivi <ashin-jo@yahoo.com.tw>
2021-03-08 21:38:32 +0800

    Initial Commit

---
+++ 猜拳遊戲/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  猜拳遊戲
+//
+//  Created by Vivi Lee on 2021/3/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var pcPlayer: UIImageView!
+    @IBOutlet weak var player: UIImageView!
+    @IBOutlet weak var result: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    //設定猜拳顯示的圖片
+    let rockImage = UIImage(named: "rock")
+    let paperImage = UIImage(named: "paper")
+    let scissorImage = UIImage(named: "scissor")
+
+    //pc隨機圖片array
+    let pcPlayerImage = ["rock","paper", "scissor"]
+    
+    @IBAction func playgame(_ sender: Any) {
+        
+    }
+    
+
+}
+
