f738ce1f2017ec329160be12a4e116c91acb133d
vivi <ashin-jo@yahoo.com.tw>
2021-03-11 22:18:10 +0800

    0311

--- 猜拳遊戲/ViewController.swift
+++ 猜拳遊戲/ViewController.swift
@@ -16,15 +16,25 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    //設定猜拳顯示的圖片:)
+    //設定猜拳顯示的圖片
     let rockImage = UIImage(named: "rock")
     let paperImage = UIImage(named: "paper")
     let scissorImage = UIImage(named: "scissor")
-
     //pc隨機圖片array
     let pcPlayerImage = ["rock","paper", "scissor"]
+    func pcPlay() {
+        pcPlayer.image = UIImage(named: pcPlayerImage.randomElement()!)
+    }
+//    設定結果顯示圖片
+    let winImage = UIImage(named: "win")
+    let loseImage = UIImage(named: "lose")
+    let winwinImage = UIImage(named: "try")
+    
+
     
     @IBAction func playgame(_ sender: Any) {
+        
+        
         
     }
     
