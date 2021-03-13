1bc4cdab3f593a09bd6f064b8618a4b36a3c1247
vivi <ashin-jo@yahoo.com.tw>
2021-03-13 19:53:09 +0800

    0313

--- 猜拳遊戲/ViewController.swift
+++ 猜拳遊戲/ViewController.swift
@@ -12,6 +12,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var pcPlayer: UIImageView!
     @IBOutlet weak var player: UIImageView!
     @IBOutlet weak var result: UIImageView!
+    @IBOutlet weak var scissorBtn: UIButton!
+    @IBOutlet weak var rockBtn: UIButton!
+    @IBOutlet weak var paperBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -25,19 +29,49 @@ class ViewController: UIViewController {
     func pcPlay() {
         pcPlayer.image = UIImage(named: pcPlayerImage.randomElement()!)
     }
-//    設定結果顯示圖片
-    let winImage = UIImage(named: "win")
-    let loseImage = UIImage(named: "lose")
-    let winwinImage = UIImage(named: "try")
-    
 
-    
-    @IBAction func playgame(_ sender: Any) {
-        
-        
-        
+//    按鈕定義三種出拳
+    @IBAction func playGameAction(_ sender: UIButton) {
+        if sender == scissorBtn{
+            player.image = scissorImage
+        }
+        if sender == rockBtn{
+            player.image = rockImage
+        }
+        if sender == paperBtn{
+            player.image = paperImage
+        }
+        pcPlay()
     }
+
+    //    設定結果顯示圖片
+        let winImage = UIImage(named: "win")
+        let loseImage = UIImage(named: "lose")
+        let winwinImage = UIImage(named: "try")
+    func resultImage() {
+        if player.image == pcPlayer.image{
+            result.image =  UIImage(named: "try")
+        }
+        else if player.image == scissorImage{
+            if pcPlayerImage == "rock"{
+                result.image = UIImage(named: "lose")
+            }
+            else result.image = uiimage(named: "win")
+        }
+        else if player.image == rockImage{
+            if pcPlayerImage == UIImage(named:"paper"){
+                result.image = UIImage(named: "lose")
+            }
+            else result.image = uiimage(named: "win")
+        }
+        else if player.image == paperImage{
+            if pcPlayerImage == UIImage(named:"scissor"){
+                result.image = UIImage(named: "lose")
+            }
+            else result.image = uiimage(named: "win")
+        }
     
+    }
 
 }
 
